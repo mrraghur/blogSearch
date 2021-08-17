2822b0ffcc860bf9d661777c22a1a735f9890f78
anyonymouscoward <me@localhost>
2021-08-16 21:24:24 -0700

    Yet another fix for startscript

--- startsearchengine.sh
+++ startsearchengine.sh
@@ -1,19 +1,19 @@
-#!/usr/bin/bash
+#!/usr/bin/bash                                                                                                    
   
 set -x
 
-#Script to start typsense server, read db.sqlite data and post documents to search engine.
+source config.sh
+#Script to start typsense server, read db.sqlite data and post documents to search engine.                         
 #A different process will start the populate db.sqlite
 
 #https://stackoverflow.com/questions/18622907/only-mkdir-if-it-does-not-exist
 #Create directory if it does not exist
-TYPESENSE_DATA_DIR="/tmp/typesense-data3"
 if [ ! -d $TYPESENSE_DATA_DIR ]; then
   mkdir $TYPESENSE_DATA_DIR
 fi
 
 #Start typesense server on port 8108 (which hopefully is not occupied)
-nohup typesense-server --data-dir=/tmp/typesense-data --api-key=xyz --listen-port 8108 --enable-cors &
+nohup typesense-server --data-dir=$TYPESENSE_DATA_DIR --api-key=xyz --listen-port 8108 --enable-cors &
 
 node createSchemaBlogs.js && node createSchemaStructuredResults.js
 
@@ -21,11 +21,10 @@ node createSchemaBlogs.js && node createSchemaStructuredResults.js
 #Build frontend
 rm -r dist # If dist/ is already present
 parcel build index.html
-rm -rf /var/www/blogSearch
-mv dist/ /var/www/blogSearch/
+rm -rf /var/www/blogSearch/dist
+mv -f dist/ /var/www/blogSearch/
 
 
-cd substacksearchengine
 #Post new data to the search engine index
-source $VENVPATH/bin/activate
-python manage.py postToTypeSense
+source ${VENVPATH}/bin/activate
+python3 manage.py postToTypeSense
